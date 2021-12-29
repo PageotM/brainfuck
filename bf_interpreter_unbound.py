3f1817b4fedc17a57f9fb815f8b987f437b71ae5
MajeedAB <majeed.abdul-baki@polymtl.ca>
2021-12-29 14:59:11 -0500

    interpreteur de script compile en brainfuck fonctionnel avec +/-/>/<

--- bf_interpreter_unbound.py
+++ bf_interpreter_unbound.py
@@ -15,7 +15,7 @@ exponent = lambda a,b: f"{init(a,b)} >>- [[>]+[<]>>-] +<- [>[+>]<[<]>-] + [>] <<
 DEFAULT_SCRIPT = exponent(18,3)
 print(DEFAULT_SCRIPT)
 
-STEP_BY_STEP = True    #parametre si on veut voir toutes les etapes, appuyez enter pour passer
+STEP_BY_STEP = False    #parametre si on veut voir toutes les etapes, appuyez enter pour passer
 DISPLAY = True         #parametre pour afficher la memoire a chaque etape
 
 def process(script, memory, writePos, readPos):
@@ -123,25 +123,36 @@ def runScript(script = DEFAULT_SCRIPT, memory = [0]):
 
 
 def main():
-    gotomin1L = "+[-<+]-"
-    gotomin3R = "+++[--->+++]---"
-    balancedLogic = f"{gotomin3R}>+<{gotomin1L}"
-    ifaeq0 = f">>+<[>-] {gotomin1L} >> [-<<{balancedLogic}>>]<<"
 
-    test_process_script = f"+++++[----- + {ifaeq0} - >>>  +++++] -----"
-    memory = [-2, 1, 0, -2, 0, 0, -2, 0, 0, -5, 0, -3, 0]
-    print("RUNNING SCRIPT:\t",test_process_script)
-    runScript(test_process_script, memory)
+    gotomin2R = "++[-->++]--"
+    if_equal_0 = lambda logic: f">>+<[>-] {gotomin2R}< [-<<{logic}>>]<<"
+
+    gotomin8R3J = "++++++++[-------->>>++++++++]--------"
+    gotomin3R2J = "+++[--->>+++]---"
+    jumpWriteFlag = f">{gotomin8R3J}{gotomin3R2J}"
+    
+    gotomin8L2J = "++++++++[--------<<++++++++]--------"
+    gotomin1L3J = "+[-<<<+]-"
+    jumpReadFlag = f"{gotomin8L2J}<{gotomin1L3J}"
+
+    logic1 = f"{jumpWriteFlag} >+< {jumpReadFlag}"
+    logic2 = f"{jumpWriteFlag} >-< {jumpReadFlag}"
+    logic3 = f"{jumpWriteFlag} ->>+ {jumpReadFlag}"
+    logic4 = f"{jumpWriteFlag} -<<+ {jumpReadFlag}"
+
+
+    executeInstruction_1_4 = f">-< {if_equal_0(logic1)} >-< {if_equal_0(logic2)} >-< {if_equal_0(logic3)} >-< {if_equal_0(logic4)} >++++<"
+    
+    process_bf_script_1_4 = f">++++++++[-------- < + {executeInstruction_1_4} - >>>  >++++++++] --------<"
+    #only processes instructions 1-4, meaning +/-/>/< respectively
+
+    memory_test_1_4 = [-2, 1, 0, -2, 3, 0, -2, 2, 0, -2, 4, 0, -2, 1, 0, -2, -8, 0, -3, 0, -4, 0, -4, 0, -4, 0]
+    print("RUNNING SCRIPT:\t",process_bf_script_1_4)
+    runScript(process_bf_script_1_4, memory_test_1_4)
 
 main()
 
-# NOTE FOR NEXT STEPS:
-# * standard logic for - / + (associated to 1 and 2, not 0...)
-
-# * standard logic for > / < (easy if only positive numbers in memory)
-
-# * adapt > / < logic with double jumps, and checkpoint at end-of-script-flag (-8/-5...), to have negative numbers in memory.
-### AKA, do: "goto(-5 right) goto(-3 right double jump), MOVE WRITE FLAG TO RIGHT/LEFT (->>+ or -<<+) , goto(-5 left double jump) goto(-1 left)"
+# TODO FOR NEXT STEPS:
 
 # * standard logic for BRACKETS []!!!
 ### decide on paper if using unique flag for each pair of brackets (generated when creating memory at the beginning)
@@ -151,6 +162,22 @@ main()
 #           ** cascade de flags? aka chaque loop dans une loop se crée un flag = flag_précedent - 1,
 #                   puis on "remonte" dans les flags a chaque sortie de boucle jusqu'au flag de "pas ds une loop" or whtvr
 
-# * decide flag conventions for read / write, script / memory... (also bracket flags conventions! ^^^)
+# * decide flag conventions for read / write, script / memory... (also bracket flags conventions! ^^^) 
+# 
+# READ FLAG MUST BE BIGGER THAN MAX NUMBER OF INSTRUCTIONS AKA = -6?
 
 # * finish paper documentation for final process
+
+
+# * one day... if error??? (write tries to < too much, ends on -8 flag... terminate program!)
+
+
+
+#NOTE FUNCTION PROCESS DONE!!:
+
+# * standard logic for - / + (associated to 1 and 2, not 0...)
+
+# * standard logic for > / < (easy if only positive numbers in memory)
+
+# * adapt > / < logic with double jumps, and checkpoint at end-of-script-flag (-8/-5...), to have negative numbers in memory.
+### AKA, do: "goto(-5 right triple jump) goto(-3 right double jump), MOVE WRITE FLAG TO RIGHT/LEFT (->>+ or -<<+) , goto(-5 left double jump) goto(-1 left triple jump)"
